e5e00afb825aca780d7730cf4fe467c6b5c8c1bb
yasutaka <siesta27@gmail.com>
2020-01-04 21:45:43 +0900

    不要なコードの削除
    $("#new_message")[0].reset();
            $(".form__submit").prop("disabled", false);
    を削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -97,8 +97,7 @@ $(function(){
         });
         $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        $("#new_message")[0].reset();
-        $(".form__submit").prop("disabled", false);
+        
       }
     })
 
